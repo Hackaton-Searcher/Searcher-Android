19f1522bdb79dc8119eca0ca0cbaa963d31e7895
zxcmnb102 <zxcmnb05@naver.com>
2021-12-29 22:13:31 +0900

    add : Channel Activity, ViewModel

--- app/src/main/java/kr/hs/dgsw/hackathon/searcher_android/view/MainActivity.kt
+++ app/src/main/java/kr/hs/dgsw/hackathon/searcher_android/view/MainActivity.kt
@@ -1,14 +1,5 @@
 package kr.hs.dgsw.hackathon.searcher_android.view
 
-import android.app.Activity
-import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.viewModels
 import kr.hs.dgsw.hackathon.searcher_android.R
@@ -18,7 +9,7 @@ import kr.hs.dgsw.hackathon.searcher_android.databinding.ActivityMainBinding
 import kr.hs.dgsw.hackathon.searcher_android.viewmodel.MainViewModel
 
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
-    override val viewModel: MainViewModel by viewModels()
+    override val mViewModel: MainViewModel by viewModels()
 
     override fun observerViewModel() {
         with(mViewModel) {
