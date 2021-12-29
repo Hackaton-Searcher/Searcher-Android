974633d5bc7de3d132ebb3cef609249ce876b6d5
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-12-29 22:03:28 +0900

    ADD : ChannelItemViewModel

--- app/src/main/java/kr/hs/dgsw/hackathon/searcher_android/adapter/callback/YoutubeDiffUtilCallBack.kt
+++ app/src/main/java/kr/hs/dgsw/hackathon/searcher_android/adapter/callback/YoutubeDiffUtilCallBack.kt
@@ -1,9 +1,10 @@
 package kr.hs.dgsw.hackathon.searcher_android.adapter.callback
 
+import androidx.lifecycle.LifecycleOwner
 import androidx.recyclerview.widget.DiffUtil
 import kr.hs.dgsw.hackathon.searcher_android.model.response.YoutuberListData
 
-class YoutubeDiffUtilCallBack : DiffUtil.ItemCallback<YoutuberListData>() {
+object YoutubeDiffUtilCallBack : DiffUtil.ItemCallback<YoutuberListData>() {
     override fun areItemsTheSame(oldItem: YoutuberListData, newItem: YoutuberListData): Boolean {
         return oldItem.channelId == newItem.channelId
     }
--- app/src/main/java/kr/hs/dgsw/hackathon/searcher_android/viewmodel/item/ChannelItemViewModel.kt
+++ app/src/main/java/kr/hs/dgsw/hackathon/searcher_android/viewmodel/item/ChannelItemViewModel.kt
@@ -1,6 +1,28 @@
 package kr.hs.dgsw.hackathon.searcher_android.viewmodel.item
 
+import androidx.lifecycle.MutableLiveData
 import kr.hs.dgsw.hackathon.searcher_android.base.BaseViewModel
+import kr.hs.dgsw.hackathon.searcher_android.model.response.YoutuberData
+import kr.hs.dgsw.hackathon.searcher_android.model.response.YoutuberListData
+import kr.hs.dgsw.hackathon.searcher_android.util.SingleLiveEvent
 
 class ChannelItemViewModel : BaseViewModel() {
+    private lateinit var youtuberListData: YoutuberListData
+    val openPostEvent = SingleLiveEvent<String>()
+
+    val name = MutableLiveData<String>()
+    val thumbnail = MutableLiveData<String>()
+    val channelId = MutableLiveData<String>()
+
+    fun bind(youtuberListData: YoutuberListData) {
+        this.youtuberListData = youtuberListData
+
+        name.value = youtuberListData.channelTitle
+        thumbnail.value = youtuberListData.thumbnails
+        channelId.value = youtuberListData.channelId
+    }
+
+    fun onClickItem() {
+        openPostEvent.value = youtuberListData.channelId
+    }
 }
