fc431d338ee515f7659426d318ed24a6b0d539fe
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-12-30 10:18:21 +0900

    ADD : 서버통신

--- app/src/main/java/kr/hs/dgsw/hackathon/searcher_android/view/MainActivity.kt
+++ app/src/main/java/kr/hs/dgsw/hackathon/searcher_android/view/MainActivity.kt
@@ -1,28 +1,30 @@
 package kr.hs.dgsw.hackathon.searcher_android.view
 
-import android.app.Activity
-import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.viewModels
-import kr.hs.dgsw.hackathon.searcher_android.R
+import kr.hs.dgsw.hackathon.searcher_android.adapter.YoutubeAdapter
 import kr.hs.dgsw.hackathon.searcher_android.base.BaseActivity
-import kr.hs.dgsw.hackathon.searcher_android.base.BaseViewModel
 import kr.hs.dgsw.hackathon.searcher_android.databinding.ActivityMainBinding
+import kr.hs.dgsw.hackathon.searcher_android.model.response.ChannelList
 import kr.hs.dgsw.hackathon.searcher_android.viewmodel.MainViewModel
 
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
+
     override val viewModel: MainViewModel by viewModels()
 
     override fun observerViewModel() {
         with(mViewModel) {
-
+            val adapter = YoutubeAdapter(this@MainActivity)
+            youtuberList.observe(this@MainActivity) {
+                val list = mutableListOf<ChannelList>()
+                list.add(it)
+                Log.d("TEST_LOCATE", "$list")
+                adapter.submitList(list)
+            }
+            errorEvent.observe(this@MainActivity) {
+                Toast.makeText(this@MainActivity, "검색어를 입력해주세요.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
